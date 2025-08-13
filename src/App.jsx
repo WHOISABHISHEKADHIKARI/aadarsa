40e14ecde03fd24f40070e900cd154d3ed9bf2a5
Shyam <shyam@example.com>
2025-08-13 17:42:42 +0545

    Remove missing Brands component import and route from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import Brands from './components/Brands';
 import ServicesRedirect from './components/ServicesRedirect';
 import NotFound from './components/NotFound';
 
@@ -15,11 +14,7 @@ function App() {
             <Home />
           </Layout>
         } />
-        <Route path="/brands" element={
-          <Layout>
-            <Brands />
-          </Layout>
-        } />
+
         <Route path="/services" element={<ServicesRedirect />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
